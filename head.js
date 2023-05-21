a2ca50834a57ef93c5c66dac6fe722a551ee5dab
Mona Lisa <anastasiia.rn@gmail.com>
2023-05-21 14:18:06 -0400

    done

--- head.js
+++ head.js
@@ -1,23 +1,23 @@
-/*
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`💚 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-    //it's a regular string with additional functionality for embedding expressions. By using ${expression}, you can insert the value of any JavaScript expression or variable into the string. The expression inside ${} is evaluated and its value is inserted into the resulting string. Template literals provide a convenient way to create strings with dynamic content without having to concatenate multiple strings or use complex string manipulation.
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-console.log(assertEqual("LHL", "LHL"));
-console.log(assertEqual(1, 2));
+         //it's a regular string with additional functionality for embedding expressions. By using ${expression}, you can insert the value of any JavaScript expression or variable into the string. The expression inside ${} is evaluated and its value is inserted into the resulting string. Template literals provide a convenient way to create strings with dynamic content without having to concatenate multiple strings or use complex string manipulation.
 
-*/
+// console.log(assertEqual("LHL", "LHL"));
+// console.log(assertEqual(1, 2));
 
-let head = function (array) {
-  console.log(array[0]);
+let head = function(array) {
+  return array[0];
 };
 
-head([5, 'as', 9]);
+//console.log('head:' + array[0]);
+console.log("head is: " + head([]));
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5, 'hello', 13]), 1);
+
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
