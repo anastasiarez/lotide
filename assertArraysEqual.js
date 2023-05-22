9f426f2f92a3fe85d82faa6d36c5aa0dd894cf0c
Mona Lisa <anastasiia.rn@gmail.com>
2023-05-22 00:13:35 -0400

    commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+//Task: Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+//Instead of doing a simple === check though, this assertion function will make use of your eqArrays function for array comparison.
+
+//To check if two arrays have the same values, we can use the JSON.stringify() method to convert the arrays to strings and then compare the strings.
+
+const assertArraysEqual = function (array1, array2) {
+  if (JSON.stringify(array1) === JSON.stringify(array2)) {
+    console.log(`💚 Assertion Passed: ${JSON.stringify(array1)} === ${JSON.stringify(array2)}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${JSON.stringify(array1)} !== ${JSON.stringify(array2)}`);
+  }
+};
+
+assertArraysEqual([12, 1, 1], [1, 1, 1])
+
